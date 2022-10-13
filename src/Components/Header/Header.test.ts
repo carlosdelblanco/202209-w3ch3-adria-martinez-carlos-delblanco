a09964a1e388320c393344ce375cd3d2b67d9a6c
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-10-13 18:38:57 +0200

    Change header.ts and header.test.ts to header folder

---
+++ src/Components/Header/Header.test.ts
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+describe("Given the a header instance", () => {
+  describe("When the header is created", () => {
+    test("it should appear the title into the document", () => {});
+    const screen = document.createElement("body");
+    const tag = "h1";
+    const header = new Header(screen, tag);
+    header.render();
+    const showInScreen = screen.querySelector("h1");
+    expect(showInScreen).not.toBeNull();
+  });
+});
